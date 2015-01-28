5abf587d125059aba27e828853ee2d2e50226359
Conner Swann <me@connerswann.me>
2015-01-28 16:32:38 -0700

    updated setup.py so it works and requirements.txt with the faker package

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name='cs399-band',
     version='0.1.0',
-    description='CS399 Band',
-    long_description=open('README.rst').read(),
+    description='CS399 Theatre',
+    long_description=open('README.md').read(),
     keywords=[
         'class',
         'django',
@@ -13,8 +13,8 @@ setup(
     ],
     author='Todd Wolfson',
     author_email='todd@twolfson.com',
-    url='https://github.com/justin/cs399',
-    download_url='https://github.com/justin/cs399/archive/master.zip',
+    url='https://github.com/yourbuddyconner/cs399-theatre',
+    download_url='https://github.com/yourbuddyconner/cs399-theatre/archive/master.zip',
     packages=find_packages(),
     license='UNLICENSE',
     install_requires=open('requirements.txt').readlines(),
