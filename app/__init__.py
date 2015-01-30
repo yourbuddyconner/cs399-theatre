0bf09c77fb2b65343e4cad8df8d3a74813323744
Conner Swann <me@connerswann.me>
2015-01-29 21:35:31 -0700

    added some more shows and some images

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,34 @@ from app.models import Show
 from datetime import date
 
 if not Show.objects.all():
-	print "Seeding dB!"
+	Show.objects.create(
+		name = "Get These Cucumbers Off Of My Face",
+		description = "A heartwarming adventure, that will test the friendship of three best friends. In an alternate timeline, a masseuse, a hairdresser and a bank teller go on a crime spree.",
+		image = "/static/images/cucumber-mask.jpg",
+		start_date = date(2015, 12, 24),
+		end_date = date(2015, 1, 30)
+		)
+	Show.objects.create(
+		name = "The Thing: The 3D Experience",
+		description = "Prepare yourself for the ultimate in 3D entertainment. Special visual effects take you into the world of everyone's favorite ball of hair!",
+		image = "/static/images/glasses-hair.jpg",
+		start_date = date(2014, 12, 24),
+		end_date = date(2015, 1, 30)
+		)
+	Show.objects.create(
+		name = "It Pays To Buy Good Tea",
+		description = "From world-renownd British playwright, Gerrard Tea'n'Crumpets comes another amazing adaptation on the events of the infamous Boston Teaparty.",
+		image = "/static/images/it-pays-tea.jpg",
+		start_date = date(2015, 3, 24),
+		end_date = date(2016, 4, 5)
+		)
+	Show.objects.create(
+		name = "Sasquatch",
+		description = "The sobering tale of the World's Most Ridiculously Hairy Man. A musical adaptation.",
+		image = "/static/images/sasquatch.jpg",
+		start_date = date(2015, 4, 7),
+		end_date = date(2016, 5, 25)
+		)
 	Show.objects.create(
 		name = "Yolo Swag: The Musical",
 		description = "Too much yolo to handle.",
@@ -13,14 +40,14 @@ if not Show.objects.all():
 	Show.objects.create(
 		name = "Your Mom: The Saga Continues",
 		description = "A touching tale of intrigue and passion.",
-		image = "/media/images/shows/80x80.gif",
+		image = "/static/images/lady.jpg",
 		start_date = date(2015, 2, 28),
 		end_date = date(2015, 3, 5)
 	)
 	Show.objects.create(
 		name = "Honey I Built A World-Dominating Robot",
 		description = "A quirky family comedy about a lovable android named Norman and his lust for power.",
-		image = "/media/images/shows/80x80.gif",
+		image = "/static/images/karate.jpg",
 		start_date = date(2015, 1, 1),
 		end_date = date(2015, 4, 30)
 	)
