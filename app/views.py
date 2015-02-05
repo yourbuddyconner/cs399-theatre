2ee3b3ad2e1bd01c6a5549b3751c3cd84abfde63
interlochen <rcd53@nau.edu>
2015-02-05 14:04:23 -0700

    Adding Show pages
    not yet linked to through the site

--- app/views.py
+++ app/views.py
@@ -35,3 +35,28 @@ def giftcards(request):
     return render(request, 'giftcards.html',{
     	'slideshow_items': Show.objects.all()
     })
+
+def show1(request):
+    return render(request, 'show1.html',{
+        'slideshow_items': Show.objects.all()
+    })
+
+def show2(request):
+    return render(request, 'show2.html',{
+        'slideshow_items': Show.objects.all()
+    })
+
+def show3(request):
+    return render(request, 'show3.html',{
+        'slideshow_items': Show.objects.all()
+    })
+
+def show4(request):
+    return render(request, 'show4.html',{
+        'slideshow_items': Show.objects.all()
+    })
+
+def show5(request):
+    return render(request, 'show5.html',{
+        'slideshow_items': Show.objects.all()
+    })
